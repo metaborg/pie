073522ba5f07fe7a0098dad006b290c5dcafe6e6
Gabriel Konat <gabrielkonat@gmail.com>
2017-09-05 13:17:04 +0200

    Do not inherit from PieModule.

--- runtime/builtin/src/main/kotlin/mb/pie/runtime/builtin/Module.kt
+++ runtime/builtin/src/main/kotlin/mb/pie/runtime/builtin/Module.kt
@@ -1,25 +1,23 @@
 package mb.pie.runtime.builtin
 
 import com.google.inject.Binder
-import mb.pie.runtime.builtin.path.*
-import mb.pie.runtime.core.PieModule
+import com.google.inject.Module
+import mb.pie.runtime.builtin.path.Copy
+import mb.pie.runtime.builtin.path.Exists
+import mb.pie.runtime.builtin.path.ListContents
+import mb.pie.runtime.builtin.path.Read
+import mb.pie.runtime.builtin.path.WalkContents
 import mb.pie.runtime.core.bindBuilder
 import mb.pie.runtime.core.builderMapBinder
 
-open class PieBuiltinModule : PieModule() {
+open class PieBuiltinModule : Module {
   override fun configure(binder: Binder) {
-    super.configure(binder)
+    val builders = binder.builderMapBinder()
 
-    binder.bindBuilders()
-  }
-
-  open protected fun Binder.bindBuilders() {
-    val builders = builderMapBinder()
-
-    bindBuilder<Exists>(builders, Exists.id)
-    bindBuilder<ListContents>(builders, ListContents.id)
-    bindBuilder<WalkContents>(builders, WalkContents.id)
-    bindBuilder<Read>(builders, Read.id)
-    bindBuilder<Copy>(builders, Copy.id)
+    binder.bindBuilder<Exists>(builders, Exists.id)
+    binder.bindBuilder<ListContents>(builders, ListContents.id)
+    binder.bindBuilder<WalkContents>(builders, WalkContents.id)
+    binder.bindBuilder<Read>(builders, Read.id)
+    binder.bindBuilder<Copy>(builders, Copy.id)
   }
 }
