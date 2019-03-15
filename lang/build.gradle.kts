a08fa4acce58282b860dee71382e7430e1ff9f8c
Gabriel Konat <gabrielkonat@gmail.com>
2019-03-15 14:15:07 +0100

    Disable publishing for PIE DSL.

--- lang/build.gradle.kts
+++ lang/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
   id("org.metaborg.spoofax.gradle.langspec") version "0.1.5"
 }
+
+spoofax {
+  createPublication = false // Disable publications until PIE DSL generates Java code and depends on a stable Spoofax.
+}
