634f14e59f378ff95d5f1f044917eac77113bdaa
Gabriel Konat <gabrielkonat@gmail.com>
2019-03-18 11:49:37 +0100

    Cleanup plugin repositories.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,14 @@ rootProject.name = "pie"
 
 pluginManagement {
   repositories {
-    maven(url = "https://artifacts.metaborg.org/content/repositories/releases/")
-    maven(url = "https://artifacts.metaborg.org/content/repositories/snapshots/")
-    // Following repositories are required by Spoofax Gradle plugin.
+    // Get plugins from artifacts.metaborg.org, first.
+    maven("https://artifacts.metaborg.org/content/repositories/releases/")
+    maven("https://artifacts.metaborg.org/content/repositories/snapshots/")
+    // Required by Spoofax Gradle plugin.
     maven("https://pluto-build.github.io/mvnrepository/")
     maven("https://sugar-lang.github.io/mvnrepository/")
     maven("http://nexus.usethesource.io/content/repositories/public/")
+    // Get plugins from Gradle plugin portal.
     gradlePluginPortal()
   }
 }
