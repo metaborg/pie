eb948e88bfec46823209ad87de6b8a9234e9feda
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2019-05-09 14:44:05 +0200

    Create starting point

--- example/playground/src/main/kotlin/mb/pie/example/playground/Main.kt
+++ example/playground/src/main/kotlin/mb/pie/example/playground/Main.kt
@@ -9,62 +9,62 @@ import java.io.File
 import java.io.Serializable
 
 class CreateFile : TaskDef<File, None> {
-  override fun getId() = "CreateFile"
+    override fun getId() = "CreateFile"
 
-  override fun exec(context: ExecContext, input: File): None {
-    input.outputStream().buffered().use {
-      it.write("Hello world".toByteArray())
+    override fun exec(context: ExecContext, input: File): None {
+        input.outputStream().buffered().use {
+            it.write("Hello world".toByteArray())
+        }
+        context.provide(input)
+        return None.instance
     }
-    context.provide(input)
-    return None.instance
-  }
 }
 
 class TransformFile : TaskDef<TransformFile.Input, File> {
-  override fun getId() = "TransformFile"
+    override fun getId() = "TransformFile"
 
-  data class Input(
-    val sourceFile: File,
-    val sourceTask: STask,
-    val destinationFile: File
-  ) : Serializable
+    data class Input(
+            val sourceFile: File,
+            val sourceTask: STask,
+            val destinationFile: File
+    ) : Serializable
 
-  override fun exec(context: ExecContext, input: Input): File {
-    val (sourceFile, sourceTask, destination) = input
-    context.require(sourceTask)
-    context.require(sourceFile, FileSystemStampers.hash())
-    val sourceText = sourceFile.readText() + ", and universe!"
-    destination.outputStream().buffered().use {
-      it.write(sourceText.toByteArray())
-      it.flush()
+    override fun exec(context: ExecContext, input: Input): File {
+        val (sourceFile, sourceTask, destination) = input
+        context.require(sourceTask)
+        context.require(sourceFile, FileSystemStampers.hash())
+        val sourceText = sourceFile.readText() + ", and universe!"
+        destination.outputStream().buffered().use {
+            it.write(sourceText.toByteArray())
+            it.flush()
+        }
+        context.provide(destination)
+        return destination
     }
-    context.provide(destination)
-    return destination
-  }
 }
 
 /**
  * The main function will start up the PIE runtime and execute the build script.
  */
 fun main(args: Array<String>) {
-  val sourceFile = File(args.getOrElse(0) { "build/run/source.txt" })
-  val destinationFile = File(args.getOrElse(1) { "build/run/destination.txt" })
+    val sourceFile = File(args.getOrElse(0) { "build/run/source.txt" })
+    val destinationFile = File(args.getOrElse(1) { "build/run/destination.txt" })
 
-  val createFile = CreateFile()
-  val transformFile = TransformFile()
-  val taskDefs = MapTaskDefs()
-  taskDefs.add(createFile)
-  taskDefs.add(transformFile)
+    val createFile = CreateFile()
+    val transformFile = TransformFile()
+    val taskDefs = MapTaskDefs()
+    taskDefs.add(createFile)
+    taskDefs.add(transformFile)
 
-  val pieBuilder = PieBuilderImpl()
-  pieBuilder.withTaskDefs(taskDefs)
-  //LMDBStore.withLMDBStore(pieBuilder, File("build/run/lmdb"))
-  pieBuilder.withLogger(StreamLogger.verbose())
-  pieBuilder.build().use { pie ->
-    val fileCreatorTask = createFile.createTask(sourceFile)
-    val transformFileTask = transformFile.createTask(
-      TransformFile.Input(sourceFile, fileCreatorTask.toSerializableTask(), destinationFile))
-    val output = pie.newSession().requireTopDown(transformFileTask)
-    println("Transformed '$sourceFile' ('${sourceFile.readText()}') to '$output' ('${output.readText()}')")
-  }
+    val pieBuilder = PieBuilderImpl()
+    pieBuilder.withTaskDefs(taskDefs)
+    //LMDBStore.withLMDBStore(pieBuilder, File("build/run/lmdb"))
+    pieBuilder.withLogger(StreamLogger.verbose())
+    pieBuilder.build().use { pie ->
+        val fileCreatorTask = createFile.createTask(sourceFile)
+        val transformFileTask = transformFile.createTask(
+                TransformFile.Input(sourceFile, fileCreatorTask.toSerializableTask(), destinationFile))
+        val output = pie.newSession().requireTopDown(transformFileTask)
+        println("Transformed '$sourceFile' ('${sourceFile.readText()}') to '$output' ('${output.readText()}')")
+    }
 }
