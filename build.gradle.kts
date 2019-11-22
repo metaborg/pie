6f5920ba380e83e7587897d680913c888ac24d7b
Gabriel Konat <gabrielkonat@gmail.com>
2019-11-22 16:02:59 +0100

    Update gradle.config and Gradle wrapper version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.8"
+  id("org.metaborg.gradle.config.root-project") version "0.3.10"
   id("org.metaborg.gitonium") version "0.1.2"
   kotlin("jvm") version "1.3.21" apply false // Apply only in sub-projects.
 }
