e5289c19abc27924ee0d3c046176600b1f5bdde2
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2019-12-16 15:17:59 +0100

    Fill in the test
    Don't know if it works yet, the generated file
    has unresolved imports that need to be fixed first

--- lang.test/src/test/java/mb/pie/lang/test/SimplePieLangTest.java
+++ lang.test/src/test/java/mb/pie/lang/test/SimplePieLangTest.java
@@ -1,5 +1,7 @@
 package mb.pie.lang.test;
 
+import mb.pie.api.ExecException;
+import mb.pie.api.None;
 import mb.pie.api.PieSession;
 import mb.pie.lang.test.util.PieRunner;
 import org.junit.jupiter.api.Test;
@@ -7,11 +9,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimplePieLangTest {
-    @Test void test() {
-        final PieRunner pieRunner = new PieRunner(/* TODO: add taskdef */);
+    @Test void test() throws ExecException {
+        final main main = new main();
+        final PieRunner pieRunner = new PieRunner(main);
         try(PieSession session = pieRunner.newSession()) {
-//            final String output = session.require(/* TODO: run task */);
-//            assertEquals(output, "Hello, world!");
+            final String output = session.require(main.createTask(None.instance));
+            assertEquals(output, "Hello, world!");
         }
     }
 }
