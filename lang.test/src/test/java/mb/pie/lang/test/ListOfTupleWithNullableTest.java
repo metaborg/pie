4e7d31ba6feb4416ec639bbafc22ad70f61271ec
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2020-01-08 16:42:49 +0100

    Create test for ListOfTupleWithNullable

---
+++ lang.test/src/test/java/mb/pie/lang/test/ListOfTupleWithNullableTest.java
@@ -0,0 +1,30 @@
+package mb.pie.lang.test;
+
+import mb.pie.api.ExecException;
+import mb.pie.api.None;
+import mb.pie.api.PieSession;
+import mb.pie.lang.test.util.PieRunner;
+import mb.pie.util.Tuple3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class ListOfTupleWithNullableTest{
+    @Test void test() throws ExecException {
+        final main___error__listOfTupleWithNullable main = new main___error__listOfTupleWithNullable();
+        final PieRunner pieRunner = new PieRunner(main);
+        try(PieSession session = pieRunner.newSession()) {
+            final ArrayList<Tuple3<Integer, String, Boolean>> output = session.require(main.createTask(None.instance));
+            assertEquals(new ArrayList<>(Arrays.asList(
+            		new Tuple3<>(5, "no", null), 
+            		new Tuple3<>(7, "kittens", true), 
+            		new Tuple3<>(-50, "TTL 01:30:57.825", false), 
+            		new Tuple3<>(830, "Compilation completed: 0 errors, 0 warnings.", true)
+            )), output);
+        }
+    }
+}
