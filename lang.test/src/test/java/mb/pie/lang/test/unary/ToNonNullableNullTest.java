613b839014cebc0966ff98a44ac76d0a3e12b432
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2020-01-14 11:53:04 +0100

    Catch NullPointerException and rethrow as ExecException in toNonNullable

--- lang.test/src/test/java/mb/pie/lang/test/unary/ToNonNullableNullTest.java
+++ lang.test/src/test/java/mb/pie/lang/test/unary/ToNonNullableNullTest.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToNonNullableNullTest {
-//    @Test()
-//    void test() throws ExecException {
-//        final main_toNonNullableNull main = new main_toNonNullableNull();
-//        final PieRunner pieRunner = new PieRunner(main);
-//        try(PieSession session = pieRunner.newSession()) {
-//            assertThrows(ExecException.class, () -> {
-//                final Integer output = session.require(main.createTask(None.instance));
-//            });
-//        }
-//    }
+    @Test()
+    void test() throws ExecException {
+        final main_toNonNullableNull main = new main_toNonNullableNull();
+        final PieRunner pieRunner = new PieRunner(main);
+        try(PieSession session = pieRunner.newSession()) {
+            assertThrows(ExecException.class, () -> {
+                final Integer output = session.require(main.createTask(None.instance));
+            });
+        }
+    }
 }
