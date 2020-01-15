e738a143223dcd74030d36d61629511291c27f9a
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2020-01-15 15:33:23 +0100

    Tmp commit to move to Mac (PC power is dying)

--- lang.test/src/test/java/mb/pie/lang/test/funcDef/TwoFuncLinearTest.java
+++ lang.test/src/test/java/mb/pie/lang/test/funcDef/TwoFuncLinearTest.java
@@ -1,22 +1,28 @@
 package mb.pie.lang.test.funcDef;
 
 import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 import mb.pie.api.ExecException;
 import mb.pie.api.None;
 import mb.pie.api.PieSession;
+import mb.pie.api.TaskDefs;
 import mb.pie.lang.test.util.PieRunner;
-import mb.pie.taskdefs.guice.GuiceTaskDefs;
+import mb.pie.taskdefs.guice.GuiceTaskDefsModule;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoFuncLinearTest {
-//    @Test void test() throws ExecException {
-//        final main_twoFuncLinear main = Guice.createInjector(new TaskDefsModule_twoFuncLinear()).getProvider(main_twoFuncLinear.class).get();
-//        final PieRunner pieRunner = new PieRunner(main);
-//        try(PieSession session = pieRunner.newSession()) {
-//            final None output = session.require(main.createTask(None.instance));
-//            assertEquals(None.instance, output);
-//        }
-//    }
+    @Test void test() throws ExecException {
+    	final Injector injector = Guice.createInjector(new GuiceTaskDefsModule(), new TaskDefsModule_twoFuncLinear());
+        final main_twoFuncLinear main = injector.getInstance(main_twoFuncLinear.class);
+        final TaskDefs taskDefs = injector.getInstance(TaskDefs.class);
+        final PieRunner pieRunner = new PieRunner(taskDefs);
+        try(PieSession session = pieRunner.newSession()) {
+            final None output = session.require(main.createTask(None.instance));
+            assertEquals(None.instance, output);
+        }
+    }
 }
