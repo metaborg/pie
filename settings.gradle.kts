91ff7717fe4554f3e6d0f1d0fedd3f546869217d
Gabriel Konat <gabrielkonat@gmail.com>
2020-01-17 11:49:16 +0100

    Remove copy-pasted signatures and pretty-printer in favor of java-front dependency.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,10 @@ pluginManagement {
     // Required by several Gradle plugins (Maven central).
     maven("https://artifacts.metaborg.org/content/repositories/central/") // Maven central mirror.
     mavenCentral() // Maven central as backup.
+    // Required by spoofax.gradle plugin.
+    maven("https://pluto-build.github.io/mvnrepository/")
+    maven("https://sugar-lang.github.io/mvnrepository/")
+    maven("http://nexus.usethesource.io/content/repositories/public/")
     // Get plugins from Gradle plugin portal.
     gradlePluginPortal()
   }
