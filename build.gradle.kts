7680a6015458d98e49006374c25a17baba895f7a
Gabriel Konat <gabrielkonat@gmail.com>
2020-01-17 13:19:56 +0100

    Use spoofax.gradle 0.2.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
   id("org.metaborg.gitonium") version "0.1.2"
   
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
-  id("org.metaborg.spoofax.gradle.langspec") version "0.1.8" apply false
-  id("org.metaborg.spoofax.gradle.project") version "0.1.8" apply false
+  id("org.metaborg.spoofax.gradle.langspec") version "0.2.0" apply false
+  id("org.metaborg.spoofax.gradle.project") version "0.2.0" apply false
   kotlin("jvm") version "1.3.61" apply false
 }
 
