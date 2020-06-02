f67a5553c86bc7a671579b2702e764e71a4f4e01
Gabriel Konat <gabrielkonat@gmail.com>
2020-06-02 13:59:40 +0200

    Update Gradle plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.20"
-  id("org.metaborg.gitonium") version "0.1.2"
+  id("org.metaborg.gradle.config.root-project") version "0.3.21"
+  id("org.metaborg.gitonium") version "0.1.3"
 
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
   id("org.metaborg.spoofax.gradle.langspec") version "0.3.6" apply false
