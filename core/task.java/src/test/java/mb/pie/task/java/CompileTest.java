32a322c012637e8144d7881ccc2d8905be9342cc
Gabriel Konat <gabrielkonat@gmail.com>
2020-08-27 11:04:05 +0200

    Make Java compile test multifile.

--- core/task.java/src/test/java/mb/pie/task/java/CompileTest.java
+++ core/task.java/src/test/java/mb/pie/task/java/CompileTest.java
@@ -14,38 +14,47 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompileTest {
     private final FileSystem fileSystem = Jimfs.newFileSystem(Configuration.unix());
     private final FSResource rootDirectory = new FSResource(fileSystem.getPath("/"));
+    private final CompileJava compileJava = new CompileJava();
+    private final MapTaskDefs taskDefs = new MapTaskDefs(compileJava);
+    private final Pie pie = new PieBuilderImpl().withTaskDefs(taskDefs).build();
 
-    private FSResource createTextFile(FSResource rootDirectory, String text, String relativePath) throws IOException {
-        final FSResource resource = rootDirectory.appendRelativePath(relativePath);
+    private final FSResource projectDir = createDir(rootDirectory, "project");
+    private final FSResource sourceFileOutputDir = createDir(rootDirectory, "build/sources");
+    private final FSResource classFileOutputDir = createDir(rootDirectory, "build/classes");
+
+    CompileTest() throws IOException {}
+
+    private FSResource createDir(FSResource parent, String relativePath) throws IOException {
+        return parent.appendRelativePath(relativePath).createDirectory(true);
+    }
+
+    private FSResource createSource(FSResource parent, String text, String relativePath) throws IOException {
+        final FSResource resource = parent.appendRelativePath(relativePath);
+        resource.createParents();
         resource.writeString(text, StandardCharsets.UTF_8);
         return resource;
     }
 
+    @SafeVarargs private final <T> ArrayList<T> createList(T... items) {
+        final ArrayList<T> list = new ArrayList<>();
+        Collections.addAll(list, items);
+        return list;
+    }
+
     @Test void testCompileTask() throws Exception {
-        final CompileJava compileJava = new CompileJava();
-        final MapTaskDefs taskDefs = new MapTaskDefs(compileJava);
-        final Pie pie = new PieBuilderImpl()
-            .withTaskDefs(taskDefs)
-            .build();
-
-        final FSResource projectDir = rootDirectory.appendRelativePath("project").createDirectory(true);
-        final FSResource testPackageDir = projectDir.appendRelativePath("test").createDirectory(true);
-        final FSResource mainJavaFile = createTextFile(testPackageDir, "package test; public class Main {}", "Main.java");
-        final FSResource sourceFileOutputDir = rootDirectory.appendRelativePath("build/sources").createDirectory(true);
-        final FSResource classFileOutputDir = rootDirectory.appendRelativePath("build/classes").createDirectory(true);
-
-        final ArrayList<ResourcePath> sourceFiles = new ArrayList<>();
-        sourceFiles.add(mainJavaFile.getPath());
-        final ArrayList<ResourcePath> sourcePath = new ArrayList<>();
-        sourcePath.add(projectDir.getPath());
-        final ArrayList<ResourcePath> classPath = new ArrayList<>();
-        final ArrayList<ResourcePath> annotationProcessorPath = new ArrayList<>();
+        final FSResource mainJavaFile = createSource(projectDir, "package test; public class Main { public static void main(String [] args) { System.out.println(HelloWorld.helloWorld()); } }", "test/Main.java");
+        final FSResource helloWorldJavaFile = createSource(projectDir, "package test; public class HelloWorld { public static String helloWorld() { return \"Hello, world!\"; } }", "test/HelloWorld.java");
+        final ArrayList<ResourcePath> sourceFiles = createList(mainJavaFile.getPath());
+        final ArrayList<ResourcePath> sourcePath = createList(projectDir.getPath());
+        final ArrayList<ResourcePath> classPath = createList();
+        final ArrayList<ResourcePath> annotationProcessorPath = createList();
 
         try(MixedSession session = pie.newSession()) {
             session.require(compileJava.createTask(new CompileJava.Input(
@@ -62,5 +71,7 @@ class CompileTest {
 
         final FSResource mainClassFile = classFileOutputDir.appendRelativePath("test/Main.class");
         assertTrue(mainClassFile.exists());
+        final FSResource helloWorldClassFile = classFileOutputDir.appendRelativePath("test/HelloWorld.class");
+        assertTrue(helloWorldClassFile.exists());
     }
 }
