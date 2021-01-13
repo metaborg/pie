467a5086f8660bbb62b32cecd1ecdc685af79fa3
Gabriel Konat <gabrielkonat@gmail.com>
2021-01-13 11:29:41 +0100

    Update Gradle wrapper to 6.8. Update GitHub actions builds.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.4.2"
+  id("org.metaborg.gradle.config.root-project") version "0.4.3"
   id("org.metaborg.gitonium") version "0.1.4"
 }
 
