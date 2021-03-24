c88b2251b71915b984033d20b7469efeb4d97347
Gabriel Konat <gabrielkonat@gmail.com>
2021-03-24 16:25:33 +0100

    Add SupplierWithOrigins and optimize for for STask suppliers.

--- core/api/src/main/java/mb/pie/api/SupplierWithOrigins.java
+++ core/api/src/main/java/mb/pie/api/SupplierWithOrigins.java
@@ -4,43 +4,46 @@ import mb.pie.api.stamp.output.OutputStampers;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
-public class SupplierWithOrigin<T extends Serializable> implements Supplier<T> {
+public class SupplierWithOrigins<T extends Serializable> implements Supplier<T> {
     private final Supplier<T> supplier;
-    private final Supplier<?> origin;
+    private final ArrayList<? extends Supplier<?>> origins;
 
-    public SupplierWithOrigin(Supplier<T> supplier, Supplier<?> origin) {
+    public SupplierWithOrigins(Supplier<T> supplier, ArrayList<? extends Supplier<?>> origins) {
         this.supplier = supplier;
-        this.origin = origin;
+        this.origins = origins;
     }
 
     @Override public T get(ExecContext context) {
-        if(origin instanceof STask<?>) {
-            context.require((STask<?>)origin, OutputStampers.inconsequential());
-        } else {
-            context.require(origin);
-        }
+        origins.forEach(origin -> {
+            if(origin instanceof STask<?>) {
+                context.require((STask<?>)origin, OutputStampers.inconsequential());
+            } else {
+                context.require(origin);
+            }
+        });
         return context.require(supplier);
     }
 
     @Override public boolean equals(@Nullable Object o) {
         if(this == o) return true;
         if(o == null || getClass() != o.getClass()) return false;
-        final SupplierWithOrigin<?> that = (SupplierWithOrigin<?>)o;
+        final SupplierWithOrigins<?> that = (SupplierWithOrigins<?>)o;
         if(!supplier.equals(that.supplier)) return false;
-        return origin.equals(that.origin);
+        return origins.equals(that.origins);
     }
 
     @Override public int hashCode() {
         int result = supplier.hashCode();
-        result = 31 * result + origin.hashCode();
+        result = 31 * result + origins.hashCode();
         return result;
     }
 
     @Override public String toString() {
         return "SupplierWithOrigin{" +
             "supplier=" + supplier +
-            ", origin=" + origin +
+            ", origins=" + origins +
             '}';
     }
 }
