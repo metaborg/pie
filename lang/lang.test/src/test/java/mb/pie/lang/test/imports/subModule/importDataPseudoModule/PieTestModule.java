3feab1d0dd9a4e29449dec117f7f151758ae7ac8
Ivo Wilms <ivoatinternet@gmail.com>
2021-05-19 17:09:44 +0200

    Bugfix: do not show duplicate file errors on multiple imports in same pseudo module
    Changes
    - add `is_proxy_module` flag
    - resolveModuleListToFile returns a list of scopes
    - remove scope from resolveUniqueFunc_1
    - create resolveUniqueFunctionInScopes
    - create resolveUniqueDataInScopes
    - create createPEdge
    - renamed s_import_tree -> s_mod_proxy for proxy modules
    - Add tests for importing with pseudo modules

---
+++ lang/lang.test/src/test/java/mb/pie/lang/test/imports/subModule/importDataPseudoModule/PieTestModule.java
@@ -0,0 +1,25 @@
+package mb.pie.lang.test.imports.subModule.importDataPseudoModule;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.ElementsIntoSet;
+import mb.pie.api.TaskDef;
+import mb.pie.lang.test.imports.subModule.a.b.c.helper_function;
+
+import javax.inject.Singleton;
+import java.util.HashSet;
+import java.util.Set;
+
+@Module
+abstract class PieTestModule {
+    @Provides @mb.pie.dagger.PieScope @ElementsIntoSet
+    public static Set<TaskDef<?, ?>> provideTaskDefs(
+        main_importDataPseudoModule importDataPseudoModule,
+        helper_function helper_function
+    ) {
+        final HashSet<TaskDef<?, ?>> taskDefs = new HashSet<>(2, 1);
+        taskDefs.add(importDataPseudoModule);
+        taskDefs.add(helper_function);
+        return taskDefs;
+    }
+}
