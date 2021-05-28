e9e22292244e5921e2aa88b4762b23bb22f2f14e
Gabriel Konat <gabrielkonat@gmail.com>
2021-05-28 14:09:27 +0200

    Fix benchmark tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,3 @@ plugins {
   id("org.metaborg.gradle.config.root-project") version "0.4.4"
   id("org.metaborg.gitonium") version "0.1.4"
 }
-
-tasks {
-  register("benchmark") {
-    val includedBuild = gradle.includedBuild("pie.bench")
-    dependsOn(includedBuild.task(":runFull"), includedBuild.task(":plotToHtml"))
-  }
-}
