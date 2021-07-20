9223e9b083c45d87d607a181d6d377df8a00f148
Gabriel Konat <gabrielkonat@gmail.com>
2021-07-20 15:50:56 +0200

    Update gradle.config to 0.4.6.

--- bench/build.gradle.kts
+++ bench/build.gradle.kts
@@ -1,7 +1,7 @@
 import ru.vyarus.gradle.plugin.python.task.PythonTask
 
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.4.4"
+  id("org.metaborg.gradle.config.root-project") version "0.4.6"
   id("org.metaborg.gradle.config.java-application") version "0.4.4"
   id("org.metaborg.gitonium") version "0.1.4"
   id("ru.vyarus.use-python") version "2.3.0"
