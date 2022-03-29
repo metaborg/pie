24bfc8885cfb211fe541449d82579847afad02ba
Ivo Wilms <ivoatinternet@gmail.com>
2022-03-30 00:28:45 +0200

    Update dependencies
    gitonium 0.1.4 -> 0.1.5
    spoofax.gradle.langspec 0.5.2 -> 0.5.5
    spoofax.gradle.project 0.5.2 -> 0.5.5

--- lang/build.gradle.kts
+++ lang/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
   id("org.metaborg.gradle.config.root-project") version "0.4.7"
-  id("org.metaborg.gitonium") version "0.1.4"
+  id("org.metaborg.gitonium") version "0.1.5"
 
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
-  id("org.metaborg.spoofax.gradle.langspec") version "0.5.2" apply false
-  id("org.metaborg.spoofax.gradle.project") version "0.5.2" apply false
+  id("org.metaborg.spoofax.gradle.langspec") version "0.5.5" apply false
+  id("org.metaborg.spoofax.gradle.project") version "0.5.5" apply false
   kotlin("jvm") version "1.3.61" apply false
 }
 
