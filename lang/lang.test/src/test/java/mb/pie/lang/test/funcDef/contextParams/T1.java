ef764a8928e090fab7f934c38dd5eb511b68cca1
Ivo Wilms <ivoatinternet@gmail.com>
2022-06-03 23:29:38 +0200

    [FAILS] add e2e test for single context parameter
    New test fails, code generation not implemented yet

---
+++ lang/lang.test/src/test/java/mb/pie/lang/test/funcDef/contextParams/T1.java
@@ -0,0 +1,12 @@
+package mb.pie.lang.test.funcDef.contextParams;
+
+import javax.inject.Inject;
+
+public class T1 {
+    @Inject
+    public T1() {}
+
+    public int get() {
+        return 1;
+    }
+}
---
+++ lang/lang.test/src/test/java/mb/pie/lang/test/funcDef/contextParams/one/OneTest.java
@@ -0,0 +1,12 @@
+package mb.pie.lang.test.funcDef.contextParams.one;
+
+import mb.pie.api.ExecException;
+import org.junit.jupiter.api.Test;
+
+import static mb.pie.lang.test.util.SimpleChecker.assertTaskOutputEquals;
+
+class OneTest {
+    @Test void test() throws ExecException {
+        assertTaskOutputEquals(DaggeroneComponent.class, 0);
+    }
+}
