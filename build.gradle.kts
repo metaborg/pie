73e4f56257a42f8422b8bef66f73fd128edd3378
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-24 16:36:57 +0200

    Update to Gitonium 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
   id("org.metaborg.gradle.config.root-project") version "0.5.6"
-  id("org.metaborg.gitonium") version "1.1.0"
+  id("org.metaborg.gitonium") version "1.2.0"
 }
--- lang/build.gradle.kts
+++ lang/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("org.metaborg.gradle.config.root-project") version "0.5.6"
-  id("org.metaborg.gitonium") version "1.1.0"
+  id("org.metaborg.gitonium") version "1.2.0"
 
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
   id("org.metaborg.spoofax.gradle.langspec") version "0.5.5" apply false
