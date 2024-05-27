bc2dcd5a426c2341f7e95cdabe264ed77bf14fbe
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 16:17:59 +0200

    Update spoofax.gradle plugin version

--- lang/build.gradle.kts
+++ lang/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.metaborg.gitonium") version "1.2.0"
 
     // Set versions for plugins to use, only applying them in subprojects (apply false here).
-    id("org.metaborg.spoofax.gradle.langspec") version "0.5.5" apply false
-    id("org.metaborg.spoofax.gradle.project") version "0.5.5" apply false
+    id("org.metaborg.spoofax.gradle.langspec") version "0.5.9" apply false
+    id("org.metaborg.spoofax.gradle.project") version "0.5.9" apply false
     kotlin("jvm") version "1.3.61" apply false
 }
 
