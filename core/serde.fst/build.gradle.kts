3506b0a1792e67adf0aa665a5323383d8a34c7a8
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-06-28 19:38:05 +0200

    Open JDK packages for Java 17 tests

--- core/serde.fst/build.gradle.kts
+++ core/serde.fst/build.gradle.kts
@@ -13,6 +13,17 @@ dependencies {
 }
 
 tasks.test {
+    jvmArgs(listOf(
+        // Needed for Java 17
+        "--add-opens", "java.base/java.io=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang.invoke=ALL-UNNAMED",
+        "--add-opens", "java.base/java.math=ALL-UNNAMED",
+        "--add-opens", "java.base/java.net=ALL-UNNAMED",
+        "--add-opens", "java.base/java.text=ALL-UNNAMED",
+        "--add-opens", "java.base/java.util=ALL-UNNAMED",
+        "--add-opens", "java.base/java.util.concurrent=ALL-UNNAMED",
+    ))
     reporting {
         testLogging {
             lifecycle {
--- core/taskdefs.guice/build.gradle.kts
+++ core/taskdefs.guice/build.gradle.kts
@@ -14,3 +14,17 @@ dependencies {
     testImplementation(project(":pie.runtime"))
     testCompileOnly("org.checkerframework:checker-qual-android")
 }
+
+tasks.test {
+    jvmArgs(listOf(
+        // Needed for Java 17
+        "--add-opens", "java.base/java.io=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+        "--add-opens", "java.base/java.lang.invoke=ALL-UNNAMED",
+        "--add-opens", "java.base/java.math=ALL-UNNAMED",
+        "--add-opens", "java.base/java.net=ALL-UNNAMED",
+        "--add-opens", "java.base/java.text=ALL-UNNAMED",
+        "--add-opens", "java.base/java.util=ALL-UNNAMED",
+        "--add-opens", "java.base/java.util.concurrent=ALL-UNNAMED",
+    ))
+}
